ec4fcd4d442d6defb8ac97af59307dec9dfc6b06
sciufudean <stefan030699@yahoo.com>
2022-05-10 16:36:01 +0300

    first update

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        String word = new String("Hello");
+        String word2 = new String("Sir");
+
+        System.out.println(word + " " + word2);
+
+    }
+}
