7c854ff9d74a1e7cb6da0fe4f423733e68e05dda
Stefanutul <stefan030699@yahoo.com>
2022-05-10 18:17:27 +0300

    Update Main.java
    change 1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String args[]){
 
         String word = new String("Hello");
+		String word2 = new String("New String");
 
-        System.out.println(word);
+        System.out.println(word + word2);
 
     }
 }
